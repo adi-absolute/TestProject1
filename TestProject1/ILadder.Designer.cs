9c27e520984a35160a86f3777452cd56e93d4600
Aditya Mandrekar <adi@absolute-software.co.uk>
2012-11-19 00:51:48 +0000

    Interim commit for pushing to server

--- TestProject1/ILadder.Designer.cs
+++ TestProject1/ILadder.Designer.cs
@@ -30,11 +30,12 @@
         {
             this.button_AddGame = new System.Windows.Forms.Button();
             this.button_AddPlayer = new System.Windows.Forms.Button();
+            this.listBox_PlayerList = new System.Windows.Forms.ListBox();
             this.SuspendLayout();
             // 
             // button_AddGame
             // 
-            this.button_AddGame.Location = new System.Drawing.Point(344, 29);
+            this.button_AddGame.Location = new System.Drawing.Point(32, 277);
             this.button_AddGame.Name = "button_AddGame";
             this.button_AddGame.Size = new System.Drawing.Size(75, 23);
             this.button_AddGame.TabIndex = 0;
@@ -44,7 +45,7 @@
             // 
             // button_AddPlayer
             // 
-            this.button_AddPlayer.Location = new System.Drawing.Point(31, 29);
+            this.button_AddPlayer.Location = new System.Drawing.Point(351, 277);
             this.button_AddPlayer.Name = "button_AddPlayer";
             this.button_AddPlayer.Size = new System.Drawing.Size(75, 23);
             this.button_AddPlayer.TabIndex = 1;
@@ -52,14 +53,23 @@
             this.button_AddPlayer.UseVisualStyleBackColor = true;
             this.button_AddPlayer.Click += new System.EventHandler(this.button_AddPlayer_Click);
             // 
-            // Form1
+            // listBox_PlayerList
+            // 
+            this.listBox_PlayerList.FormattingEnabled = true;
+            this.listBox_PlayerList.Location = new System.Drawing.Point(351, 23);
+            this.listBox_PlayerList.Name = "listBox_PlayerList";
+            this.listBox_PlayerList.Size = new System.Drawing.Size(83, 238);
+            this.listBox_PlayerList.TabIndex = 2;
+            // 
+            // ILadder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(452, 417);
+            this.Controls.Add(this.listBox_PlayerList);
             this.Controls.Add(this.button_AddPlayer);
             this.Controls.Add(this.button_AddGame);
-            this.Name = "Form1";
+            this.Name = "ILadder";
             this.Text = "Form1";
             this.ResumeLayout(false);
 
@@ -69,6 +79,7 @@
 
         private System.Windows.Forms.Button button_AddGame;
         private System.Windows.Forms.Button button_AddPlayer;
+        private System.Windows.Forms.ListBox listBox_PlayerList;
 
     }
 }
